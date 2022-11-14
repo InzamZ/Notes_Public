4b11865611a508ca90c2cd1a2c8794ae32c3cf99
InzamZ <me@inzamz.top>
2022-11-14 20:32:16 +0800

    :lipstick: 增加格式化脚本

---
+++ format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "尝试使用 zhlint 格式化所有 markdown 文档"
+
+cnt=0
+# 递归遍历所有 markdown 文件
+for file in `(find . -name "**.md")`; do
+    echo "正在格式化 $file"
+    zhlint $file --fix
+    if [ $? -eq 1 ]; then
+        let cnt+=1
+    fi
+done
+
+echo "格式化完成，共格式化 $cnt 个文件"
