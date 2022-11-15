5b2ac0f2073ab7b16e65ec721845012c5dc989ef
InzamZ <me@inzamz.top>
2022-11-15 12:50:43 +0800

    :art: 优化目录结构

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,19 +18,19 @@ export default {
                 text: '计算机网络',
                 items: [
                     { text: '计算机网络', link: '/CoursesNotes/计算机网络/第一章_计算机网络' },
-                    { text: '第一章:计算机网络体系结构', link: '/CoursesNotes/计算机网络/第一章_计算机网络体系结构' },
+                    { text: '第一章：计算机网络体系结构', link: '/CoursesNotes/计算机网络/第一章_计算机网络体系结构' },
                 ]
             }],
             '/CoursesNotes/网络攻防技术/': [{
                 text: '网络攻防技术',
                 items: [
                     { text: '网络攻防技术', link: '/CoursesNotes/网络攻防技术/第4章_传输层安全协议TLS' },
-                    { text: '第4章:传输层安全协议TLS', link: '/CoursesNotes/网络攻防技术/第4章_传输层安全协议TLS' },
-                    { text: '第5章:无线局域网安全WLAN', link: '/CoursesNotes/网络攻防技术/第5章_无线局域网安全WLAN' },
-                    { text: '第6章:渗透测试-情报收集技术', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-情报收集技术' },
-                    { text: '第6章:渗透测试-漏洞扫描技术', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-漏洞扫描技术' },
-                    { text: '第6章:渗透测试-Metasploit框架', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-Metasploit框架' },
-                    { text: '第7章:漏洞挖掘与利用', link: '/CoursesNotes/网络攻防技术/第7章_漏洞挖掘与利用' },
+                    { text: '第4章：传输层安全协议TLS', link: '/CoursesNotes/网络攻防技术/第4章_传输层安全协议TLS' },
+                    { text: '第5章：无线局域网安全WLAN', link: '/CoursesNotes/网络攻防技术/第5章_无线局域网安全WLAN' },
+                    { text: '第6章：渗透测试-情报收集技术', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-情报收集技术' },
+                    { text: '第6章：渗透测试-漏洞扫描技术', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-漏洞扫描技术' },
+                    { text: '第6章：渗透测试-Metasploit框架', link: '/CoursesNotes/网络攻防技术/第6章_渗透测试-Metasploit框架' },
+                    { text: '第7章：漏洞挖掘与利用', link: '/CoursesNotes/网络攻防技术/第7章_漏洞挖掘与利用' },
                 ]
             }],
             '/ReadingNotes/': [{
@@ -44,7 +44,7 @@ export default {
                 text: 'ReadingNotes',
                 items: [
                     { text: 'Linux内核设计与实现', link: '/ReadingNotes/Linux内核设计与实现/第一章_Linux内核简介' },
-                    { text: '第一章:Linux内核简介', link: '/ReadingNotes/Linux内核设计与实现/第一章_Linux内核简介' },
+                    { text: '第一章：Linux内核简介', link: '/ReadingNotes/Linux内核设计与实现/第一章_Linux内核简介' },
                 ]
             }]
         }
