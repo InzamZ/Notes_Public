75630352f3fa04ef8cfc95682c181b0bbe7d375c
InzamZ <me@inzamz.top>
2022-11-16 22:17:04 +0800

    docs(linux 内核设计与实现): 更新笔记 第二章：从内核出发

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -72,6 +72,7 @@ export default {
                 items: [
                     { text: 'Linux 内核设计与实现', link: '/ReadingNotes/Linux内核设计与实现/' },
                     { text: '第一章：Linux 内核简介', link: '/ReadingNotes/Linux内核设计与实现/第一章_Linux内核简介' },
+                    { text: '第二章：从内核出发', link: '/ReadingNotes/Linux内核设计与实现/第二章_从内核出发' },
                 ]
             }]
         }
