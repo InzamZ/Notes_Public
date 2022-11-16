c36660fa8a033c9db7e1e6f58a4623c656f3d589
InzamZ <me@inzamz.top>
2022-11-16 23:17:45 +0800

    style(zhlint): ignore some unnessary output

--- script/format.sh
+++ script/format.sh
@@ -4,10 +4,10 @@ echo "尝试使用 zhlint 格式化所有 markdown 文档"
 cnt=0
 # 递归遍历所有 markdown 文件
 for file in `(find ./docs -name "**.md")`; do
-    zhlint $file > /dev/null
+    zhlint $file >/dev/null 2>&1
     if [ $? -eq 1 ]; then
         echo "正在格式化 $file"
-        zhlint $file --fix > /dev/null
+        zhlint $file --fix >/dev/null 2>&1
         let cnt+=1
     fi
 done
