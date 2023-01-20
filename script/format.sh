bcc39cddeb11f48d8facb03a29a03f9474beed61
InzamZ <me@inzamz.top>
2023-01-21 01:41:01 +0800

    build: ignore formatting XCPC solution

--- script/format.sh
+++ script/format.sh
@@ -4,6 +4,9 @@ echo "尝试使用 zhlint 格式化所有 markdown 文档"
 cnt=0
 # 递归遍历所有 markdown 文件
 for file in `(find ./docs -name "**.md")`; do
+    if [[ "${file:0:11}" == "./docs/XCPC" ]]; then
+        continue
+    fi
     zhlint $file >/dev/null 2>&1
     if [ $? -eq 1 ]; then
         echo "正在格式化 $file"
